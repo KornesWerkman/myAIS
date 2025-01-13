ce8f91867e7fe95b6963c3c31b5a0d18e0a23a64
Kornes Werkman <kornes@duck.com>
2025-01-13 19:22:31 +0800

    Add some message statistics

--- tracks/tracks.py
+++ tracks/tracks.py
@@ -1,6 +1,7 @@
 import pathlib
 import uuid
 from datetime import datetime, timezone
+import ais
 
 import tomllib
 
@@ -16,6 +17,24 @@ with open(toml_file, "rb") as configs:
 
 
 class CommonNavigationBlock:
+    """Class that handles the common navigation block, the first 3 fields
+    of any VDO/VDM message:
+     - The Message ID
+     - The Repeat indicator
+     - The User ID (or source ID, MMSI)
+
+    This class adds some meta data as well, such as:
+     - A UUID for each target
+     - Creation and update times
+    """
+
+    msg_received: dict = {
+        "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0,
+        "9": 0, "10": 0, "11": 0, "12": 0, "13": 0, "14": 0, "15": 0,
+        "16": 0, "17": 0, "18": 0, "19": 0, "20": 0, "21": 0, "22": 0,
+        "23": 0, "24": 0,"25": 0, "26": 0, "27": 0
+    }
+
     def __init__(self, user_id: str) -> None:
         # -- Info from common navigation block
         self._user_id = user_id
@@ -36,6 +55,7 @@ class CommonNavigationBlock:
     def message_id(self, value: int) -> None:
         self._ts_updated = datetime.now(timezone.utc)
         self._message_id = int(value)
+        CommonNavigationBlock.msg_received[str(self._message_id)] += 1
 
     @property
     def repeat_indicator(self) -> int:
@@ -61,6 +81,21 @@ class CommonNavigationBlock:
     def user_id(self) -> str:
         return self._user_id
 
+    def msg_statistics() -> str:
+        """ Method that prints a bar diagram displaying the how many
+        of each message type were received
+        """
+
+        tnom = sum(
+            CommonNavigationBlock.msg_received.values()
+            )
+        for k, v in CommonNavigationBlock.msg_received.items():
+            print(f"Message {k:2}: ",
+                  f"{"#"*(int(50 * (v / tnom)))} ",
+                  f"{v / tnom:.0%} ({v})"
+                  )
+
+
     def __str__(self) -> str:
         return (
             f"MMSI: {self._user_id}, Track ID: {self.track_id}\n"
@@ -69,7 +104,7 @@ class CommonNavigationBlock:
         )
 
 
-class Track(CommonNavigationBlock):
+class Target(CommonNavigationBlock):
     def __init__(
         self,
         user_id,
@@ -246,10 +281,44 @@ class Track(CommonNavigationBlock):
 
 
 # -- --------------------------------------------------------------------------
-tracks = {}
-mmsi = "265547250"
-tracks[mmsi] = Track(mmsi)
-tracks[mmsi].message_id = 1
-tracks[mmsi].repeat_indicator = 1
+# -- messages borrowed from https://www.maritec.co.za/aisvdmvdodecoding
+# --
 
-print(tracks[mmsi])
+messages: list = [
+    "!AIVDM,1,1,,A,13u?etPv2;0n:dDPwUM1U1Cb069D,0*23",
+    "!AIVDM,1,1,,A,400TcdiuiT7VDR>3nIfr6>i00000,0*78",
+    "!AIVDM,2,1,0,A,58wt8Ui`g??r21`7S=:22058<v05Htp000000015>8OA;0sk,0*7B",
+    "!AIVDM,2,2,0,A,eQ8823mDm3kP00000000000,2*5D",
+    "!AIVDM,1,1,4,B,6>jR0600V:C0>da4P106P00,2*02",
+    "!AIVDM,2,1,9,B,61c2;qLPH1m@wsm6ARhp<ji6ATHd<C8f=Bhk>34k;S8i=3To,0*2C",
+    "!AIVDM,2,2,9,B,Djhi=3Di<2pp=34k>4D,2*03",
+    "!AIVDM,1,1,1,B,8>h8nkP0Glr=<hFI0D6??wvlFR06EuOwgwl?wnSwe7wvlOw?sAwwnSGmwvh0,0*17",
+    "!AIVDO,1,1,,A,95M2oQ@41Tr4L4H@eRvQ;2h20000,0*0D",
+    "!AIVDM,1,1,,B,;8u:8CAuiT7Bm2CIM=fsDJ100000,0*51",
+    "!AIVDM,1,1,,B,>>M4fWA<59B1@E=@,0*17",
+    "!AIVDM,1,1,,A,B6CdCm0t3`tba35f@V9faHi7kP06,0*58",
+    "!AIVDM,2,1,0,B,C8u:8C@t7@TnGCKfm6Po`e6N`:Va0L2J;06HV50JV?SjBPL3,0*28",
+    "!AIVDM,2,2,0,B,11RP,0*17",
+    "!AIVDO,2,1,5,B,E1c2;q@b44ah4ah0h:2ab@70VRpU<Bgpm4:gP50HH`Th`QF5,0*7B",
+    "!AIVDO,2,2,5,B,1CQ1A83PCAH0,0*60",
+    "!AIVDO,1,1,,B,H1c2;qA@PU>0U>060<h5=>0:1Dp,2*7D",
+    "!AIVDO,1,1,,B,H1c2;qDTijklmno31<<C970`43<1,0*28",
+    "!AIVDM,1,1,,A,KCQ9r=hrFUnH7P00,0*41",
+    "!AIVDM,1,1,,B,KC5E2b@U19PFdLbMuc5=ROv62<7m,0*16",
+    "!AIVDM,1,1,,B,K5DfMB9FLsM?P00d,0*70"
+]
+
+for message in messages:
+    payload = message.split(",")[5]
+    # payload = ais.payload_to_binary(payload)
+    print(payload)
+
+
+targets = {}
+mmsi = "265547250"
+targets[mmsi] = Target(mmsi)
+targets[mmsi].message_id = 1
+targets[mmsi].repeat_indicator = 1
+
+print(targets[mmsi])
+CommonNavigationBlock.msg_statistics()
